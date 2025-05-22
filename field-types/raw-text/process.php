2408715c424182cd1c38c72beabe235d4cbea9c7
Tim Buckingham <mr@timbuckingham.com>
2025-05-21 20:26:49 -0400

    Fixing JSON inference

--- field-types/raw-text/process.php
+++ field-types/raw-text/process.php
@@ -1,2 +1,7 @@
 <?php
-	$field["output"] = $field["input"];
+	// Was erroneously re-decoded from JSON to an array
+	if (is_array($field["input"])) {
+		$field["output"] = json_encode($field["input"], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+	} else {
+		$field["output"] = $field["input"];
+	}
